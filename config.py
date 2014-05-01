f3f763aaa105d972160f501655e2164da1f15cd0
Eric Fernance <beanie@purplebeanie.com>
2014-05-01 18:36:52 +1000

    untrack config
    Signed-off-by: Eric Fernance <beanie@purplebeanie.com>

--- config.py
+++
@@ -1,3 +0,0 @@
-kobo_db_loc = "/Volumes/KOBOeReader/.kobo/KoboReader.sqlite"
-evernote_dev_token = "get_your_developer_token_from_evernote"
-evernote_notestore = "get_your_evernote_notestore_from_evernote"
